56ca7a02361f94c7a11a6fc5d85c99677aa39a01
Robert F. Dickerson <rfdickerson@gmail.com>
2016-01-21 11:42:36 -0600

    added Phoenix as a dependency

--- Package.swift
+++ Package.swift
@@ -2,7 +2,9 @@ import PackageDescription
 
 let package = Package(
     name: "PhoenixRedis",
-    dependencies: [ ],
+    dependencies: [ 
+    .Package(url: "git@github.ibm.com:ibmswift/Phoenix.git", majorVersion: 1),
+],
     testDependencies: [
         .Package(url: "git@github.ibm.com:ibmswift/PhoenixTestFramework.git", majorVersion: 0)
         ]
