a565657401d444b5567f5df8b63b6c35484bf332
Shmuel Kallner <kallner@il.ibm.com>
2016-01-27 17:25:39 +0200

    Fix dependencies. HiRedis should be a Redis dependency not a Phoenix one

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftRedis",
     dependencies: [ 
-    .Package(url: "git@github.ibm.com:ibmswift/Phoenix.git", majorVersion: 0),
+        .Package(url: "git@github.ibm.com:ibmswift/PhoenixHiredis.git", majorVersion: 0),
+        .Package(url: "git@github.ibm.com:ibmswift/Phoenix.git", majorVersion: 0),
 ],
     testDependencies: [
         .Package(url: "git@github.ibm.com:ibmswift/PhoenixTestFramework.git", majorVersion: 0)
