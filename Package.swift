2575739194e6c4f9794aeb9b79cca4b65cefb26a
Daniel Firsht <dfirsht@us.ibm.com>
2016-03-23 14:05:40 -0500

    Updated to new version.

--- Package.swift
+++ Package.swift
@@ -20,6 +20,6 @@ let package = Package(
     name: "SwiftRedis",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-hiredis.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", majorVersion: 0, minor: 6),
     ]
 )
