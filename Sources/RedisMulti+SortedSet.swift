195deee86b14c49cd9dce6b3448ed463ad357ad3
Robert Deans <robert.deans@uk.ibm.com>
2016-10-10 11:13:40 +0100

    IBM-Swift/Kitura#499 Added Transactional sorted set API's

---
+++ Sources/RedisMulti+SortedSet.swift
@@ -0,0 +1,387 @@
+/**
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import Foundation
+
+/// Extend RedisMulti by adding the Sorted Set operations
+extension RedisMulti {
+    
+    /// Add an ZADD command to the "transaction"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter tuples: A list of tuples containing a score and value to be added to the sorted set.
+    @discardableResult
+    public func zadd(_ key: String, tuples: (Int, String)...) -> RedisMulti {
+        zaddArrayOfScoreMembers(key, tuples: tuples)
+        return self
+    }
+    
+    /// Add an ZADD command to the "transaction"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter tuples: A list of tuples containing a score and value to be added to the sorted set.
+    @discardableResult
+    public func zaddArrayOfScoreMembers(_ key: String, tuples: [(Int, String)]) -> RedisMulti {
+        var command = [RedisString("ZADD"), RedisString(key)]
+        for tuple in tuples {
+            command.append(RedisString(tuple.0))
+            command.append(RedisString(tuple.1))
+        }
+        queuedCommands.append(command)
+        return self
+    }
+    
+    /// Add an ZADD command to the "transaction"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter tuples: A list of tuples containing a score and value to be added to the sorted set.
+    @discardableResult
+    public func zadd(_ key: String, tuples: (Int, RedisString)...) -> RedisMulti {
+        zaddArrayOfScoreMembers(key, tuples: tuples)
+        return self
+    }
+    
+    /// Add an ZADD command to the "transaction"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter tuples: A list of tuples containing a score and value to be added to the sorted set.
+    @discardableResult
+    public func zaddArrayOfScoreMembers(_ key: String, tuples: [(Int, RedisString)]) -> RedisMulti {
+        var command = [RedisString("ZADD"), RedisString(key)]
+        for tuple in tuples {
+            command.append(RedisString(tuple.0))
+            command.append(tuple.1)
+        }
+        queuedCommands.append(command)
+        return self
+    }
+    
+    /// Add an ZCARD command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    @discardableResult
+    public func zcard(_ key: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZCARD"), RedisString(key)])
+        return self
+    }
+    
+    /// Add an ZCOUNT command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter min: The minimum score to count from the set.
+    /// - Parameter max: The maximum score to count from the set.
+    @discardableResult
+    public func zcount(_ key: String, min: String, max: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZCOUNT"), RedisString(key), RedisString(min), RedisString(max)])
+        return self
+    }
+    
+    /// Add an ZINCRBY command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter increment: The amount to increment the member by.
+    /// - Parameter member: The member to increment.
+    @discardableResult
+    public func zincrby(_ key: String, increment: Int, member: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZINCRBY"), RedisString(key), RedisString(increment), RedisString(member)])
+        return self
+    }
+    
+    /// Add an ZINCRBY command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter increment: The amount to increment the member by.
+    /// - Parameter member: The member to increment.
+    @discardableResult
+    public func zincrby(_ key: String, increment: Int, member: RedisString) -> RedisMulti {
+        queuedCommands.append([RedisString("ZINCRBY"), RedisString(key), RedisString(increment), member])
+        return self
+    }
+    
+    /// Add an ZINTERSTORE command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter increment: The amount to increment the member by.
+    /// - Parameter member: The member to increment.
+    @discardableResult
+    public func zinterstore(_ destination: String, numkeys: Int, keys: String..., weights: [Int] = [], aggregate: String = "") -> RedisMulti {
+        var command = [RedisString("ZINTERSTORE"), RedisString(destination), RedisString(numkeys)]
+        for key in keys {
+            command.append(RedisString(key))
+        }
+        if weights.count > 0 {
+            command.append(RedisString("WEIGHTS"))
+            for weight in weights {
+                command.append(RedisString(weight))
+            }
+        }
+        if aggregate != "" {
+            command.append(RedisString("AGGREGATE"))
+            command.append(RedisString(aggregate))
+        }
+        queuedCommands.append(command)
+        return self
+    }
+    
+    /// Add an ZLEXCOUNT command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter min: The minimum score to count from the set.
+    /// - Parameter max: The maximum score to count from the set.
+    @discardableResult
+    public func zlexcount(_ key: String, min: String, max: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZLEXCOUNT"), RedisString(key), RedisString(min), RedisString(max)])
+        return self
+    }
+    
+    /// Add an ZRANGE command to the "transactions"
+    /// - Parameter key: The key.
+    /// - Parameter increment: The amount to increment the member by.
+    /// - Parameter member: The member to increment.
+    @discardableResult
+    public func zrange(_ key: String, start: Int, stop: Int) -> RedisMulti {
+        queuedCommands.append([RedisString("ZRANGE"), RedisString(key), RedisString(start), RedisString(stop)])
+        return self
+    }
+    
+    /// Add an ZRANGEBYLEX command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter min: The minimum score to count from the set.
+    /// - Parameter max: The maximum score to count from the set.
+    @discardableResult
+    public func zrangebylex(_ key: String, min: String, max: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZRANGEBYLEX"), RedisString(key), RedisString(min), RedisString(max)])
+        return self
+    }
+    
+    /// Add an ZRANGEBYSCORE command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter min: The minimum score to count from the set.
+    /// - Parameter max: The maximum score to count from the set.
+    @discardableResult
+    public func zrangebyscore(_ key: String, min: String, max: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZRANGEBYSCORE"), RedisString(key), RedisString(min), RedisString(max)])
+        return self
+    }
+    
+    /// Add an ZRANK command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter member: The member to get the rank of.
+    @discardableResult
+    public func zrank(_ key: String, member: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZRANK"), RedisString(key), RedisString(member)])
+        return self
+    }
+    
+    /// Add an ZREM command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter members: The list of the member(s) to remove.
+    @discardableResult
+    public func zrem(_ key: String, members: String...) -> RedisMulti {
+        zremArrayOfMembers(key, members: members)
+        return self
+    }
+    
+    /// Add an ZREM command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter members: The list of the member(s) to remove.
+    @discardableResult
+    public func zremArrayOfMembers(_ key: String, members: [String]) -> RedisMulti {
+        var command = [RedisString("ZREM"), RedisString(key)]
+        for member in members {
+            command.append(RedisString(member))
+        }
+        queuedCommands.append(command)
+        return self
+    }
+    
+    /// Add an ZREMRANGEBYLEX command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter min: The minimum score to remove from the set.
+    /// - Parameter max: The maximum score to remove from the set.
+    @discardableResult
+    public func zremrangebylex(_ key: String, min: String, max: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZREMRANGEBYLEX"), RedisString(key), RedisString(min), RedisString(max)])
+        return self
+    }
+    
+    /// Add an ZREMRANGEBYRANK command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter start: The starting index to remove from the set.
+    /// - Parameter stop: The ending index to remove from the set.
+    @discardableResult
+    public func zremrangebyrank(_ key: String, min: String, max: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZREMRANGEBYRANK"), RedisString(key), RedisString(min), RedisString(max)])
+        return self
+    }
+
+    /// Add an ZREMRANGEBYSCORE command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter min: The minimum score to remove from the set.
+    /// - Parameter max: The maximum score to remove from the set.
+    @discardableResult
+    public func zremrangebyscore(_ key: String, min: String, max: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZREMRANGEBYSCORE"), RedisString(key), RedisString(min), RedisString(max)])
+        return self
+    }
+    
+    /// Add an ZREVRANGE command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter start: The starting index to return from the set.
+    /// - Parameter stop: The stoping index to return from the set.
+    /// - Parameter withscores: Whether or not to return scores as well.
+    @discardableResult
+    public func zrevrange(_ key: String, start: Int, stop: Int, withscores: Bool = false) -> RedisMulti {
+        if !withscores {
+            queuedCommands.append([RedisString("ZREVRANGE"), RedisString(key), RedisString(start), RedisString(stop)])
+        } else {
+            queuedCommands.append([RedisString("ZREVRANGE"), RedisString(key), RedisString(start), RedisString(stop), RedisString("WITHSCORES")])
+        }
+        return self
+    }
+
+    /// Add an ZREVRANGEBYLEX command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter min: The minimum score to remove from the set.
+    /// - Parameter max: The maximum score to remove from the set.
+    @discardableResult
+    public func zrevrangebylex(_ key: String, min: String, max: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZREVRANGEBYLEX"), RedisString(key), RedisString(min), RedisString(max)])
+        return self
+    }
+    
+    /// Add an ZREVRANGEBYSCORE command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter min: The minimum score to remove from the set.
+    /// - Parameter max: The maximum score to remove from the set.
+    @discardableResult
+    public func zrevrangebyscore(_ key: String, min: String, max: String, withscore: Bool = false) -> RedisMulti {
+        if !withscore {
+            queuedCommands.append([RedisString("ZREVRANGEBYSCORE"), RedisString(key), RedisString(min), RedisString(max)])
+        } else {
+            queuedCommands.append([RedisString("ZREVRANGEBYSCORE"), RedisString(key), RedisString(min), RedisString(max), RedisString("WITHSCORES")])
+        }
+        return self
+    }
+    
+    /// Add an ZREVRANK command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter member: The member to get the rank of.
+    @discardableResult
+    public func zrevrank(_ key: String, member: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZREVRANK"), RedisString(key), RedisString(member)])
+        return self
+    }
+    
+    /// Add an ZCAN command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter cursor: iterator
+    /// - Parameter match: glob-style pattern
+    /// - parameter count: The amount of work that should be done at every call in order
+    ///                   to retrieve elements from the collection.
+    @discardableResult
+    public func zscan(_ key: String, cursor: Int, match: String? = nil, count: Int? = nil) -> RedisMulti {
+        let ZSCAN = RedisString("ZSCAN")
+        let MATCH = RedisString("MATCH")
+        let COUNT = RedisString("COUNT")
+        
+        if let match = match, let count = count {
+            queuedCommands.append([ZSCAN, RedisString(key), RedisString(cursor), MATCH, RedisString(match), COUNT, RedisString(count)])
+        }
+        if let match = match {
+            queuedCommands.append([ZSCAN, RedisString(key), RedisString(cursor), MATCH, RedisString(match)])
+        } else if let count = count {
+            queuedCommands.append([ZSCAN, RedisString(key), RedisString(cursor), COUNT, RedisString(count)])
+        } else {
+            queuedCommands.append([ZSCAN, RedisString(key), RedisString(cursor)])
+        }
+        return self
+    }
+    
+    /// Add an ZCAN command to the "transactions"
+    ///
+    /// - Parameter key: The key.
+    /// - Parameter member: The member to get the score from.
+    @discardableResult
+    public func zscore(_ key: String, member: String) -> RedisMulti {
+        queuedCommands.append([RedisString("ZSCORE"), RedisString(key), RedisString(member)])
+        return self
+    }
+    
+    /// Add an ZCAN command to the "transactions"
+    ///
+    /// - Parameter destination: The destination where the result will be stored.
+    /// - Parameter numkeys: The number of keys to union.
+    /// - Parameter keys: The keys.
+    /// - Parameter weights: A multiplication factor for each input sorted set.
+    /// - Parameter aggregate: Specify how the results of the union are aggregated.
+    @discardableResult
+    public func zunionstore(_ destination: String, numkeys: Int, keys: String..., weights: [Int] = [], aggregate: String = "") -> RedisMulti {
+        zunionstoreWithArray(destination, numkeys: numkeys, keys: keys, weights: weights, aggregate: aggregate)
+        return self
+    }
+    
+    /// Add an ZCAN command to the "transactions"
+    ///
+    /// - Parameter destination: The destination where the result will be stored.
+    /// - Parameter numkeys: The number of keys to union.
+    /// - Parameter keys: The keys.
+    /// - Parameter weights: A multiplication factor for each input sorted set.
+    /// - Parameter aggregate: Specify how the results of the union are aggregated.
+    /// - Parameter callback: The callback function, the Int will contain the number of elements
+    ///                       in the resulting sorted set at destination.
+    ///                       NSError will be non-nil if an error occured.
+    @discardableResult
+    public func zunionstoreWithArray(_ destination: String, numkeys: Int, keys: [String], weights: [Int], aggregate: String) -> RedisMulti {
+        queuedCommands.append(appendValues(operation: "ZUNIONSTORE", destination, numkeys: numkeys, keys: keys, weights: weights, aggregate: aggregate))
+        return self
+    }
+    
+    //Appends all the values into a String array ready to be used for issueCommandInArray()
+    private func appendValues(operation: String, _ destination: String, numkeys: Int, keys: [String], weights: [Int], aggregate: String) -> [RedisString] {
+        var command = [RedisString(operation)]
+        command.append(RedisString(destination))
+        command.append(RedisString(numkeys))
+        for key in keys {
+            command.append(RedisString(key))
+        }
+        if weights.count > 0 {
+            command.append(RedisString("WEIGHTS"))
+            for weight in weights {
+                command.append(RedisString(weight))
+            }
+        }
+        if aggregate != "" {
+            command.append(RedisString("AGGREGATE"))
+            command.append(RedisString(aggregate))
+        }
+        return command
+    }
+
+}
---
+++ Tests/SwiftRedisTests/TestTransactionsPart6.swift
@@ -0,0 +1,173 @@
+/**
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import SwiftRedis
+
+import Foundation
+import XCTest
+
+//Tests the Set operations
+public class TestTransactionsPart6: XCTestCase {
+    static var allTests: [(String, (TestTransactionsPart6) -> () throws -> Void)] {
+        return [
+            ("test_keyManipulation", test_keyManipulation),
+            ("test_MemberManipulation", test_MemberManipulation),
+            ("test_ranges", test_ranges)
+        ]
+    }
+    
+    let key1 = "test1"
+    let key2 = "test2"
+    let key3 = "test3"
+    let key4 = "test4"
+    let key5 = "test5"
+    
+    let expVal1 = "Hi ho, hi ho"
+    let expVal2 = "it's off to test"
+    let expVal3 = "we go"
+    let expVal4 = "Testing"
+    let expVal5 = "testing 1 2 3"
+    
+    func test_keyManipulation() {
+        setupTests() {
+            let multi = redis.multi()
+            multi.zadd(self.key1, tuples: (1, self.expVal1), (1, self.expVal2), (1, self.expVal3))
+            multi.zadd(self.key2, tuples: (1, self.expVal2), (1, self.expVal4))
+            multi.zunionstore(self.key3, numkeys: 2, keys: self.key1, self.key2)
+            multi.zinterstore(self.key2, numkeys: 2, keys: self.key1, self.key3)
+            
+            multi.exec() {(response: RedisResponse) in
+                if  let nestedResponses = self.baseAsserts(response: response, count: 4) {
+                    XCTAssertEqual(nestedResponses[0], RedisResponse.IntegerValue(3), "zadd only added \(nestedResponses[0]) tuples not 3")
+                    XCTAssertEqual(nestedResponses[1], RedisResponse.IntegerValue(2), "zadd only added \(nestedResponses[1]) tuples not 2")
+                    XCTAssertEqual(nestedResponses[2], RedisResponse.IntegerValue(4), "There are only \(nestedResponses[2]) members not 4")
+                    XCTAssertEqual(nestedResponses[3], RedisResponse.IntegerValue(3), "There are only \(nestedResponses[3]) members not 3")
+                }
+            }
+        }
+    }
+    
+    func test_MemberManipulation() {
+        setupTests() {
+            let multi = redis.multi()
+            multi.zadd(self.key1, tuples: (1, self.expVal1), (1, self.expVal2), (1, self.expVal3))
+            multi.zrem(self.key1, members: self.expVal2)
+            multi.zcard(self.key1)
+            multi.zincrby(self.key1, increment: 3, member: RedisString(self.expVal1))
+            multi.zscore(self.key1, member: self.expVal3)
+            
+            multi.exec() {(response: RedisResponse) in
+                if  let nestedResponses = self.baseAsserts(response: response, count: 5) {
+                    XCTAssertEqual(nestedResponses[0], RedisResponse.IntegerValue(3), "zadd only added \(nestedResponses[0]) tuples")
+                    XCTAssertEqual(nestedResponses[1], RedisResponse.IntegerValue(1), "Should have removed \(self.expVal2)")
+                    XCTAssertEqual(nestedResponses[2], RedisResponse.IntegerValue(2), "There are only \(nestedResponses[2]) members not 2")
+                    XCTAssertEqual(nestedResponses[3], RedisResponse.StringValue(RedisString("4")))
+                    XCTAssertEqual(nestedResponses[4], RedisResponse.StringValue(RedisString("1")))
+                }
+            }
+        }
+    }
+    
+    func test_ranges() {
+        let expectedRange = [RedisResponse.StringValue(RedisString(self.expVal2)),
+                             RedisResponse.StringValue(RedisString(self.expVal3)),
+                             RedisResponse.StringValue(RedisString(self.expVal4))]
+
+        let expectedRangeLex = [RedisResponse.StringValue(RedisString("b")),
+                                RedisResponse.StringValue(RedisString("c")),
+                                RedisResponse.StringValue(RedisString("d")),
+                                RedisResponse.StringValue(RedisString("e")),
+                                RedisResponse.StringValue(RedisString("f"))]
+
+        let expectedRangeScore = [RedisResponse.StringValue(RedisString(self.expVal2)),
+                                  RedisResponse.StringValue(RedisString(self.expVal3)),
+                                  RedisResponse.StringValue(RedisString(self.expVal4))]
+        
+        let expectedRevRange = [RedisResponse.StringValue(RedisString(self.expVal5)),
+                                RedisResponse.StringValue(RedisString(self.expVal4)),
+                                RedisResponse.StringValue(RedisString(self.expVal3)),
+                                RedisResponse.StringValue(RedisString(self.expVal2)),
+                                RedisResponse.StringValue(RedisString(self.expVal1))]
+        
+        let expectedRevRangeLex = [RedisResponse.StringValue(RedisString("c")),
+                                RedisResponse.StringValue(RedisString("b")),
+                                RedisResponse.StringValue(RedisString("a"))]
+        
+        setupTests() {
+            let multi = redis.multi()
+            multi.zadd(self.key1, tuples: (1, self.expVal1), (2, self.expVal2), (3, self.expVal3), (4, self.expVal4), (5, self.expVal5))
+            multi.zadd(self.key2, tuples: (1, "a"), (1, "b"), (1, "c"), (1, "d"), (1, "e"), (1, "f"), (1, "g"))
+            multi.zrange(self.key1, start: 1, stop: 3)
+            multi.zrangebylex(self.key2, min: "[aaa", max: "(g")
+            multi.zrangebyscore(self.key1, min: "2", max: "(5")
+            
+            multi.zrevrange(self.key1, start: 0, stop: -1)
+            multi.zrevrank(self.key1, member: self.expVal4)
+            multi.zrevrangebylex(self.key2, min: "[c", max: "-")
+            multi.zrevrangebyscore(self.key1, min: "+inf", max: "-inf")
+            
+            multi.exec() {(response: RedisResponse) in
+                if  let nestedResponses = self.baseAsserts(response: response, count: 9) {
+                    XCTAssertEqual(nestedResponses[0], RedisResponse.IntegerValue(5), "Added only \(nestedResponses[0]) not 5")
+                    XCTAssertEqual(nestedResponses[1], RedisResponse.IntegerValue(7), "Added only \(nestedResponses[1]) not 7")
+                    XCTAssertEqual(nestedResponses[2], RedisResponse.Array(expectedRange))
+                    XCTAssertEqual(nestedResponses[3], RedisResponse.Array(expectedRangeLex))
+                    XCTAssertEqual(nestedResponses[4], RedisResponse.Array(expectedRangeScore))
+                    XCTAssertEqual(nestedResponses[5], RedisResponse.Array(expectedRevRange), "Not equal")
+                    XCTAssertEqual(nestedResponses[6], RedisResponse.IntegerValue(1))
+                    XCTAssertEqual(nestedResponses[7], RedisResponse.Array(expectedRevRangeLex))
+                    XCTAssertEqual(nestedResponses[8], RedisResponse.Array(expectedRevRange))
+                }
+            }
+        }
+    }
+    
+    
+    private func baseAsserts(response: RedisResponse, count: Int) -> [RedisResponse]? {
+        switch(response) {
+        case .Array(let responses):
+            XCTAssertEqual(responses.count, count, "Number of nested responses wasn't \(count), was \(responses.count)")
+            for  nestedResponse in responses {
+                switch(nestedResponse) {
+                case .Error:
+                    XCTFail("Nested transaction response was a \(nestedResponse)")
+                    return nil
+                default:
+                    break
+                }
+            }
+            return responses
+        default:
+            XCTFail("EXEC response wasn't an Array response. Was \(response)")
+            return nil
+        }
+    }
+    
+    private func setupTests(callback: () -> Void) {
+        connectRedis() {(error: NSError?) in
+            if error != nil {
+                XCTFail("Could not connect to Redis")
+                return
+            }
+            
+            redis.del(self.key1, self.key2, self.key3, self.key4) {(deleted: Int?, error: NSError?) in
+                XCTAssertNil(error, "\(error != nil ? error!.localizedDescription : "")")
+                
+                callback()
+            }
+        }
+    }
+}
